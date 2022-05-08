b0caedcc67cce10609ebbd9ca703fcbe951e505c
AlexGnSoft <gnuskin.ae@gmail.com>
2022-05-08 15:37:05 +0100

    Setting up a real test

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class Constants {
+
+    public static class TimeoutVariables{
+        public static final long IMPLICITY_WAIT = 30;
+        public static final long PAGE_LOAD_WAIT = 30;
+    }
+}
